b00cf9c1330eb7a24e72cc85024ef8a16ac5a551
Yusuf Alamu Musa <volunux@gmail.com>
2023-05-28 16:02:41 +0100

    add shopping cart and checkout dtos

--- src/main/java/com/yulam/acalib/model/dto/order/ShoppingCart.java
+++ src/main/java/com/yulam/acalib/model/dto/order/ShoppingCart.java
@@ -1,4 +1,18 @@
 package com.yulam.acalib.model.dto.order;
 
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShoppingCart {
+
+  private Set<OrderItemDto> orderItemDtos;
+  private Integer totalNumberOfItems;
+  private Double totalAmount;
+  private Integer totalQuantityOfItems;
 }
